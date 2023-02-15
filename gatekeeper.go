7ef47baddb808d2bc59cbdec392f2f267051a077
Racxshan Nagalingam <510460@student.fontys.nl>
2023-02-15 21:33:36 +0100

    gatekeeper v3

--- gatekeeper.go
+++ gatekeeper.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-func main() {
+func boodschap() {
 	tijd := time.Now()
 	tijdinuur := tijd.Hour()
 	if tijdinuur >= 7 && tijdinuur < 12 {
@@ -16,5 +16,27 @@ func main() {
 		fmt.Println("Goedenavond! Welkom bij Fonteyn Vakantieparken")
 	} else if tijdinuur > 23 && tijdinuur < 7 {
 		fmt.Println("Sorry, de parkeerplaats is ’s nachts gesloten")
+	}
+}
+
+func main() {
+	kenteken := [10]string{"AB-123-CD", "XY-456-Z", "WZ-789-Y", "MN-111-P", "QA-222-B", "KL-333-J", "HI-444-N", "VG-555-M", "FC-666-T", "BU-777-K"}
+
+	var input string
+	fmt.Println("Hallo! Wat is uw Kenteken?")
+	fmt.Scan(&input)
+
+	staaterbij := false
+	for i := 0; i < len(kenteken); i++ {
+		if input == kenteken[i] {
+			staaterbij = true
+			break
+		}
+	}
+
+	if staaterbij {
+		boodschap()
+	} else {
+		fmt.Println("U heeft helaas geen toegang tot het parkeerterrein")
 	}
 }

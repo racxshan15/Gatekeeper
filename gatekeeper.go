e8919f65a691255b8514fcb32a43894abd93653e
Racxshan Nagalingam <510460@student.fontys.nl>
2023-02-15 21:32:46 +0100

    gatekeeper v3

--- gatekeeper.go
+++ gatekeeper.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	fmt.Println("Welkom bij Fonteyn Vakantieparken")
+	tijd := time.Now()
+	tijdinuur := tijd.Hour()
+	if tijdinuur >= 7 && tijdinuur < 12 {
+		fmt.Println("Goedemorgen! Welkom bij Fonteyn Vakantieparken")
+	} else if tijdinuur >= 12 && tijdinuur < 18 {
+		fmt.Println("Goedemiddag! Welkom bij Fonteyn Vakantieparken")
+	} else if tijdinuur > 18 && tijdinuur < 23 {
+		fmt.Println("Goedenavond! Welkom bij Fonteyn Vakantieparken")
+	} else if tijdinuur > 23 && tijdinuur < 7 {
+		fmt.Println("Sorry, de parkeerplaats is ’s nachts gesloten")
+	}
 }
